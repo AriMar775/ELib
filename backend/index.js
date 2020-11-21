bb41cec058373088872854e3f2fa39ffba178406
Arina Marchenko <arinamar7705@gmail.com>
2020-11-21 20:00:04 +0300

    added backend part and changed structure

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// npm run dev
