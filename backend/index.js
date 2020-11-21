2ee929e9308571b4a5cfdc58af2f0c7e3ed475e5
Arina Marchenko <arinamar7705@gmail.com>
2020-11-21 23:01:04 +0300

    added mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(3000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// npm run dev
